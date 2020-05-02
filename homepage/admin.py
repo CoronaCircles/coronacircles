8e4159f732f6dbed1fb0ac19005ea8aa7fd3c12d
Henrik Kröger <hedwig@riseup.net>
2020-05-02 02:27:08 +0200

    simple pages through flatpages

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Testimonial, CarouselItem
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from ckeditor.widgets import CKEditorWidget
 
 
 class TestimonialAdmin(admin.ModelAdmin):
@@ -14,3 +20,22 @@ class CarouselItemAdmin(admin.ModelAdmin):
 
 
 admin.site.register(CarouselItem, CarouselItemAdmin)
+
+
+class FlatPageCustom(FlatPageAdmin):
+    formfield_overrides = {models.TextField: {"widget": CKEditorWidget}}
+    fieldsets = (
+        (None, {"fields": ("url", "title", "content",)}),
+        (
+            _("Advanced options"),
+            {
+                "classes": ("collapse",),
+                "fields": ("registration_required", "template_name"),
+            },
+        ),
+    )
+    list_filter = ("registration_required",)
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageCustom)
