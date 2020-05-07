c65a89ca4555cb3e1c4dfb4f7589efd5e2efdf67
Henrik Kröger <hedwig@riseup.net>
2020-05-08 01:12:31 +0200

    fix code formatting in admin.py

--- circles/admin.py
+++ circles/admin.py
@@ -5,16 +5,11 @@ from modeltranslation.admin import TranslationAdmin
 from .models import Event, MailTemplate
 
 
-class MailTemplateAdmin(admin.ModelAdmin):
-    list_display = ('type','language_code')
 @admin.register(Event)
-
-
 class EventAdmin(admin.ModelAdmin):
     list_display = ("uuid", "start", "language", "host")
 
+
 @admin.register(MailTemplate)
-
-
 class MailTemplateAdmin(TranslationAdmin):
     list_display = ("type",)
