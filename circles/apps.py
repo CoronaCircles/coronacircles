408119323dd4d7977804d8827f3a2d83026e1722
Henrik Kröger <hedwig@riseup.net>
2020-05-07 18:00:48 +0200

    Make app verbose names translatable

--- circles/apps.py
+++ circles/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class CirclesConfig(AppConfig):
-    name = 'circles'
+    name = "circles"
+    verbose_name = _("Circles")
